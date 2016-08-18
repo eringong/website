0abe6be0b6d15a78cc0296d88da3564df57f4a55
Erin Gong <gongerin@gmail.com>
2016-08-18 13:37:05 -0700

    added real data

---
+++ uec_lm/js/lm_data.js
@@ -0,0 +1,268 @@
+relationship = {
+  "1": {
+    "text": "Roseville rate payers", 
+    "lm_type": "relationship", 
+    "expanded_text": "Business and residential customers<br>More homeowners than other communities (more autonomy to make decisions about utility use)<br>Higher education, income, older"
+  }, 
+  "2": {
+    "text": "Multi-generational family visitors", 
+    "lm_type": "relationship", 
+    "expanded_text": "none"
+  }, 
+  "3": {
+    "text": "Senior citizens", 
+    "lm_type": "relationship", 
+    "expanded_text": "growing population in Roseville"
+  }, 
+  "4": {
+    "text": "Event goers and workshop attendees", 
+    "lm_type": "relationship", 
+    "expanded_text": "none"
+  }, 
+  "5": {
+    "text": "Social media followers", 
+    "lm_type": "relationship", 
+    "expanded_text": "none"
+  }, 
+  "6": {
+    "text": "Educational community", 
+    "lm_type": "relationship", 
+    "expanded_text": "TK-6 grades<br>Local teachers and other educators<br>Parents of students"
+  }, 
+  "7": {
+    "text": "Business community", 
+    "lm_type": "relationship", 
+    "expanded_text": "Chamber of Commerce<br>Event vendors<br>Sponsors<br>Event co-hosts<br>Current partners/relationships (Entercom, Energy 2011 Inc.)<br>Potential partners/relationships"
+  }, 
+  "8": {
+    "text": "News media", 
+    "lm_type": "relationship", 
+    "expanded_text": "none"
+  }, 
+  "9": {
+    "text": "Volunteers", 
+    "lm_type": "relationship", 
+    "expanded_text": "Current<br>Potential"
+  }, 
+  "10": {
+    "text": "Local government", 
+    "lm_type": "relationship", 
+    "expanded_text": "City offices<br>Local, state, regional environmental agencies"
+  }, 
+  "11": {
+    "text": "Utility Departments", 
+    "lm_type": "relationship", 
+    "expanded_text": "Roseville Electric Utility<br>Roseville Environmental Utilities"
+  }, 
+  "12": {
+    "text": "Library, Sports Center, Mahany Park", 
+    "lm_type": "relationship", 
+    "expanded_text": "none"
+  }, 
+  "13": {
+    "text": "Staff", 
+    "lm_type": "relationship", 
+    "expanded_text": "none"
+  }
+};
+
+resources = {
+  "16": {
+    "text": "Comic books", 
+    "lm_type": "resources", 
+    "expanded_text": "none"
+  }, 
+  "17": {
+    "text": "Revenue", 
+    "lm_type": "resources", 
+    "expanded_text": "Direct sponsorships and donations<br>Indirect sponsorships<br>Non-grant funded tours<br>Programs (summer camps, workshop)<br>Store sales"
+  }, 
+  "14": {
+    "text": "Location/space", 
+    "lm_type": "resources", 
+    "expanded_text": "Mahany Park<br>Indoor exhibition space<br>Outdoor IDEAscape<br>Workshop/classroom facilities"
+  }, 
+  "15": {
+    "text": "Marketing assets", 
+    "lm_type": "resources", 
+    "expanded_text": "New website<br>Social media presence<br>Collateral"
+  }
+};
+
+activities = {
+  "18": {
+    "text": "Planet Protectors school programs", 
+    "lm_type": "activities", 
+    "expanded_text": "TK-6<br>Pre- and post-viist material<br>State curriculum aligned<br>Free add-ons: library tour, zero-waste lunch dump<br>Topics covering: Energy efficiency, waste reduction, watershed protection (program titles: Electron flow, Elements of waste, H2Own, Hydro Heroes, Keepin it clean, Power Patrol, Stuff busters, Water warriors)<br>Program evaluation"
+  }, 
+  "19": {
+    "text": "Group tours", 
+    "lm_type": "activities", 
+    "expanded_text": "Children<br>Boy and Girl Scouts<br>Homeschool<br>Afterschool<br>Daycare<br>Adult<br>Exhibit Hall Tours<br>LEED Building Facility Tours<br>Pre- and post- materials<br>Program evaluation"
+  }, 
+  "20": {
+    "text": "Programs", 
+    "lm_type": "activities", 
+    "expanded_text": "Free Prek program (monthly, 2 hrs, mornings, dropin)<br>Family events (summer, Big Trucks Summer, free)<br>Gardening workshops (modest fee)<br>Green Gardening at Home Series<br>Adult workshops (Recycled knitting, modest fee)<br>Electric workshops (solar)<br>Summer camps<br>LED Holiday Lights exchange<br>E-waste and paper shredding drop-offs and events<br>Partnerships to do programs (regional agencies, businesses, organizations -- instruction, marketing, underwriting)<br>Annual program evaluation"
+  }, 
+  "21": {
+    "text": "Special events", 
+    "lm_type": "activities", 
+    "expanded_text": "Count Watt\u2019s Spooktacular<br>Celebrate the Earth festival<br>Greener Gardens Tour & DIY Expo<br>Curby\u2019s Waste-a-Palooza<br>Co-host 4 off-site events"
+  }, 
+  "22": {
+    "text": "Self-directed visits to exhibit space", 
+    "lm_type": "activities", 
+    "expanded_text": "none"
+  }, 
+  "23": {
+    "text": "Marketing", 
+    "lm_type": "activities", 
+    "expanded_text": "Increasing social media presence (FB, Twitter, review sites)<br>Consistent media presence with print media"
+  }, 
+  "24": {
+    "text": "Recharging of physical space", 
+    "lm_type": "activities", 
+    "expanded_text": "Exhibit reinstallation<br>IDEAscape construction"
+  }, 
+  "25": {
+    "text": "Pursue grant funding", 
+    "lm_type": "activities", 
+    "expanded_text": "capital improvements, program development, program expansion and sponsorships"
+  }, 
+  "26": {
+    "text": "Become 501c3 or friends group", 
+    "lm_type": "activities", 
+    "expanded_text": "Membership program<br>Tax deductible contributions"
+  }, 
+  "27": {
+    "text": "Partner/stakeholder relationship building", 
+    "lm_type": "activities", 
+    "expanded_text": "Mutually beneficial relationships with businesses, service groups, city offices, schools and colleges"
+  }, 
+  "28": {
+    "text": "Staff", 
+    "lm_type": "activities", 
+    "expanded_text": "5 full-time staff<br>Strong staff, PD"
+  }, 
+  "29": {
+    "text": "Volunteer development", 
+    "lm_type": "activities", 
+    "expanded_text": "Current volunteer corps<br>Volunteer program: min 8 hrs per month; otherwise be special event volunteer"
+  }
+};
+
+outputs = {
+  "32": {
+    "text": "Exhibit space", 
+    "lm_type": "outputs", 
+    "expanded_text": "# of people visited (34K, goal: increase by 18%)<br># of times content is refreshed (goal: 4x per year)"
+  }, 
+  "33": {
+    "text": "Programs", 
+    "lm_type": "outputs", 
+    "expanded_text": "# of programs held<br># of participants<br>Ratio of participants to programs (efficiency)<br># of workshop participants (350)<br># of special event participants (13K)"
+  }, 
+  "34": {
+    "text": "Partnerships", 
+    "lm_type": "outputs", 
+    "expanded_text": "# of active partnerships"
+  }, 
+  "35": {
+    "text": "Revenue", 
+    "lm_type": "outputs", 
+    "expanded_text": "Direct sponsorships and donations ($15K)<br>Indirect sponsorships ($4K)<br>Non-grant funded tours ($8K)<br>Programs (summer camps, workshop) ($12K)<br>Store sales ($9K)<br>E-waste/coin funnel for ideascape: $14K"
+  }, 
+  "30": {
+    "text": "Schools", 
+    "lm_type": "outputs", 
+    "expanded_text": "# of school tours<br># of students visiting on school tours (5K, goal 10K/yr)<br># of schools who come on field trips<br>% schools in/out of Roseville<br># of teachers whose classes come on field trips<br># of tour programs (8)"
+  }, 
+  "31": {
+    "text": "Volunteers", 
+    "lm_type": "outputs", 
+    "expanded_text": "# volunteers (goal: increase by 25%)<br># volunteer opportunities (goal: increase by 20%)<br># hrs served/volunteer"
+  }
+};
+
+outcomes = {
+  "36": {
+    "text": "Learn: I am part of many systems.", 
+    "lm_type": "outcomes", 
+    "expanded_text": "none"
+  }, 
+  "37": {
+    "text": "Learn: My utilities do some complex things", 
+    "lm_type": "outcomes", 
+    "expanded_text": "none"
+  }, 
+  "38": {
+    "text": "Learn: I am part of the natural world ", 
+    "lm_type": "outcomes", 
+    "expanded_text": "Man-made systems impact the environment around us."
+  }, 
+  "39": {
+    "text": "Learn: This is what I can do.", 
+    "lm_type": "outcomes", 
+    "expanded_text": "Tools to make a difference in home and community"
+  }, 
+  "40": {
+    "text": "Attitude: I play a role", 
+    "lm_type": "outcomes", 
+    "expanded_text": "none"
+  }, 
+  "41": {
+    "text": "Attitude: Amazement ", 
+    "lm_type": "outcomes", 
+    "expanded_text": "scale, connectivitiy, excellence in delivering utilities)"
+  }, 
+  "42": {
+    "text": "Attitude: Trust", 
+    "lm_type": "outcomes", 
+    "expanded_text": "none"
+  }, 
+  "43": {
+    "text": "Attitude: Savings", 
+    "lm_type": "outcomes", 
+    "expanded_text": "saving $ plus saving environment"
+  }, 
+  "44": {
+    "text": "Attitude: Worry", 
+    "lm_type": "outcomes", 
+    "expanded_text": "about condition of our environmnet, community, bottom line)"
+  }, 
+  "45": {
+    "text": "Attitude: Pride, community", 
+    "lm_type": "outcomes", 
+    "expanded_text": "none"
+  }, 
+  "46": {
+    "text": "Attitude: Home", 
+    "lm_type": "outcomes", 
+    "expanded_text": "none"
+  }
+};
+
+impact = {
+  "48": {
+    "text": "Inspire stewardship at home", 
+    "lm_type": "impact", 
+    "expanded_text": "\u201cI can act today and make a difference.\u201d"
+  }, 
+  "49": {
+    "text": "Reflect on the interconnectedness between utility, customer, community, and planet", 
+    "lm_type": "impact", 
+    "expanded_text": "\u201cI had no idea how much these systems are all connected to me, and that my utilities are doing so much for me and the future of my community.\u201d<br>\u201cI should do the right thing because it benefits me, but also because it benefits my community when we all work together.\u201d"
+  }, 
+  "50": {
+    "text": "Recognized leader for utility education and interpretation.", 
+    "lm_type": "impact", 
+    "expanded_text": "none"
+  }, 
+  "47": {
+    "text": "Amaze with what our utilities do every day", 
+    "lm_type": "impact", 
+    "expanded_text": "\u201cI\u2019m amazed by and grateful for what great things can be done by great people, both in my community and in my utilities.\u201d"
+  }
+};
+
--- uec_lm/js/myTutorial.js
+++ uec_lm/js/myTutorial.js
@@ -1,5 +1,5 @@
 outcomes = {
-	'01':{'outcome_text':'This is our first outcome','type':'1'},
+	'01':{'outcome_text':'First outcome','type':'1'},
 	'02':{'outcome_text':'This is outcome #2','type':'1'},
 	'03':{'outcome_text':'Here is another outcome','type':'2'}
 };
