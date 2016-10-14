b020f4713adc30c3ed485b1606602ed38b7fe6ee
Erin Gong <gongerin@gmail.com>
2016-10-13 19:57:53 -0700

    add ECi logic model folder

---
+++ eci_logic_model/js/gen_js.py
@@ -0,0 +1,51 @@
+'''
+This script reads in the logic model data downloaded from Google Sheets as a csv.
+It creates:
+1. .js file with variables for each of the columns in the LM and the boxes w/in each column (lm_data.js)
+2. .js file with variables for each of the node lists used to sort/filter the columns based on items in the relationships column (node_lists.js)
+'''
+
+import pandas as pd
+import json
+
+#import and prep df
+df = pd.read_csv("eci_lm_data.csv")
+df.expanded_text = df.expanded_text.str.replace('\n', '<br>')
+df['id_short_tag'] = '#node-div-' + df['id_short'].astype(str)
+
+#create lm_data.js
+
+outfile = file('lm_data.js','w')
+
+for element in list(df.lm_type.unique()):
+    element_dict = {}
+
+    new_df = df[df['lm_type'] == element]
+    for i, row in new_df.iterrows():
+        new_item_value = {
+            'lm_type': row['lm_type'],
+            'text': row['text'],
+            'expanded_text': row['expanded_text'],
+            'grant_type': row['type'],
+#             'node_connections':row['node_connections'],
+        }
+        new_item_key = row['id_short']
+        element_dict[new_item_key] = new_item_value
+    
+    outfile.write(str(element)+' = '+json.dumps(element_dict, indent = 2)+';\n\n')
+
+outfile.close()
+
+
+#create node_lists.js
+
+outfile_node_list = file('node_lists.js','w')
+
+for col in df.columns:
+    if col[:8] == "connect_":
+        df[col].fillna(df.id_short_tag, inplace=True)
+        node_list = df[col].tolist()
+        node_list = [x for x in node_list if x != 1.0]
+        outfile_node_list.write('node_list_' + str(col)+' = '+json.dumps(node_list, indent = 2)+';\n\n')
+
+outfile_node_list.close()
---
+++ eci_logic_model/js/lm_data.js
@@ -0,0 +1,297 @@
+relationships = {
+  "artists_eciprofs": {
+    "text": "Artists & Early Childhood Professionals", 
+    "lm_type": "relationships", 
+    "expanded_text": "none", 
+    "grant_type": "IMLS"
+  }, 
+  "families": {
+    "text": "Families", 
+    "lm_type": "relationships", 
+    "expanded_text": "current audience + underserved families", 
+    "grant_type": "both"
+  }, 
+  "preschools": {
+    "text": "Preschools", 
+    "lm_type": "relationships", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "parents": {
+    "text": "Parents", 
+    "lm_type": "relationships", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "museums": {
+    "text": "Museums", 
+    "lm_type": "relationships", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }
+};
+
+outcomes = {
+  "families_outcome_joy": {
+    "text": "Families have a sense of joy and delight, esp. w/in family -- enhanced sense of well-being", 
+    "lm_type": "outcomes", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "families_outcome_kready": {
+    "text": "Children more school ready - academic, social, emotional, behavior skills", 
+    "lm_type": "outcomes", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "parent_outcome_perception": {
+    "text": "Parents have a changed perception of museum as a place for them and their family", 
+    "lm_type": "outcomes", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "artists_eciprofs_outcome_newways": {
+    "text": "Artists and Early Childhood Professionals think in new ways", 
+    "lm_type": "outcomes", 
+    "expanded_text": "none", 
+    "grant_type": "IMLS"
+  }, 
+  "parent_outcome_knowledge": {
+    "text": "Parents increase knowledge, skills, motivation to engage w/ children around art", 
+    "lm_type": "outcomes", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "preschools_outcome_exposearts": {
+    "text": "Preschools increase opportunity to expose kids to developmentally appropriate arts experiences", 
+    "lm_type": "outcomes", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "museums_bestpractices": {
+    "text": "Contributing to best practices in early learning in museums", 
+    "lm_type": "outcomes", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "families_outcome_social": {
+    "text": "Families have social interaction - especially w/ different groups", 
+    "lm_type": "outcomes", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }
+};
+
+barriers = {
+  "barriers_different_parenting": {
+    "text": "Differing approaches to parenting  (value systems related  to discipline, obedience, exploration, discovery)", 
+    "lm_type": "barriers", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "barriers_art_not_interesting": {
+    "text": "Arts experiences not appealing/interesting", 
+    "lm_type": "barriers", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "barriers_look_family_friendly": {
+    "text": "Do not look family friendly", 
+    "lm_type": "barriers", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "barriers_parking": {
+    "text": "Parking fee", 
+    "lm_type": "barriers", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "barriers_time": {
+    "text": "Schedules of working parents", 
+    "lm_type": "barriers", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "barriers_staff_knowledge": {
+    "text": "Knowledge of how to deal with multi-age families when focusing on preK", 
+    "lm_type": "barriers", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "barriers_economic": {
+    "text": "Admission cost", 
+    "lm_type": "barriers", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "barriers_museum_not_children": {
+    "text": "Museum not for children", 
+    "lm_type": "barriers", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "barriers_reliable_transport": {
+    "text": "Access to reliable transportation for families", 
+    "lm_type": "barriers", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "barriers_mulilin_staff": {
+    "text": "Lack of multi-lingual staff and signage", 
+    "lm_type": "barriers", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "barriers_welcome_families": {
+    "text": "Space/routine/assets around welcoming families", 
+    "lm_type": "barriers", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "barriers_lack_leisure": {
+    "text": "Lack of  leisure time", 
+    "lm_type": "barriers", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "barriers_art_not_important": {
+    "text": "Arts experiences not important to school readiness -- no value", 
+    "lm_type": "barriers", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "barriers_cultural_personal": {
+    "text": "Museum as exclusive/elite", 
+    "lm_type": "barriers", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "barriers_staff_training": {
+    "text": "Lack of training, awareness around building new audiences", 
+    "lm_type": "barriers", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }
+};
+
+assets = {
+  "assets_staff": {
+    "text": "Crocker preK Education staff", 
+    "lm_type": "assets", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "assets_funding": {
+    "text": "Funding for preK and underserved community outreach", 
+    "lm_type": "assets", 
+    "expanded_text": "First 5<br>IMLS<br>Irvine (Block by Block)", 
+    "grant_type": "both"
+  }, 
+  "assets_artparenting": {
+    "text": "Art of Parenting", 
+    "lm_type": "assets", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "assets_rel_w_artists": {
+    "text": "Growing relationships with community artists", 
+    "lm_type": "assets", 
+    "expanded_text": "none", 
+    "grant_type": "both"
+  }, 
+  "assets_current_programs": {
+    "text": "Current family programs", 
+    "lm_type": "assets", 
+    "expanded_text": "Wee Wednesday<br>Artful Total<br>Baby Loves Art", 
+    "grant_type": "both"
+  }, 
+  "assets_current_fam_resources": {
+    "text": "Current family resources", 
+    "lm_type": "assets", 
+    "expanded_text": "Story Trail I<br>Tips for Tots<br>Art Packs", 
+    "grant_type": "both"
+  }, 
+  "assets_current_prek_resources": {
+    "text": "Current preschool resources", 
+    "lm_type": "assets", 
+    "expanded_text": "A is for Art", 
+    "grant_type": "both"
+  }
+};
+
+activities = {
+  "activities_preK_program_improvements": {
+    "text": "Continuous improvements on BLA, Artful Tot, WW", 
+    "lm_type": "activities", 
+    "expanded_text": "none", 
+    "grant_type": "First5"
+  }, 
+  "activities_storytrail": {
+    "text": "Improve Story Trail", 
+    "lm_type": "activities", 
+    "expanded_text": "Create Story Trail II<br>Translate Story Trail I Spanish", 
+    "grant_type": "First5"
+  }, 
+  "activities_reduce_economic_barriers": {
+    "text": "Reduce economic barriers to visiting the museum", 
+    "lm_type": "activities", 
+    "expanded_text": "Distribute museum passes in underserved areas", 
+    "grant_type": "both"
+  }, 
+  "activities_preK_exhibits": {
+    "text": "Create Art Spots", 
+    "lm_type": "activities", 
+    "expanded_text": "Interactive artist-designed exhibits", 
+    "grant_type": "IMLS"
+  }, 
+  "activities_collaboration": {
+    "text": "Deep collaboration b/w artists, museum staff, and early childhood professionals", 
+    "lm_type": "activities", 
+    "expanded_text": "none", 
+    "grant_type": "IMLS"
+  }, 
+  "activities_aforart": {
+    "text": "A is for Art", 
+    "lm_type": "activities", 
+    "expanded_text": "none", 
+    "grant_type": "First5"
+  }, 
+  "activities_parenting_resources": {
+    "text": "Create parenting resources", 
+    "lm_type": "activities", 
+    "expanded_text": "Expand Art of Parenting<br>Refresh Art Packs<br>Translate Tips for Tots into Spanish", 
+    "grant_type": "First5"
+  }, 
+  "activities_concierge": {
+    "text": "Concierge", 
+    "lm_type": "activities", 
+    "expanded_text": "Training, concierge table<br>Amenities for families", 
+    "grant_type": "both"
+  }, 
+  "activities_reduce_time_barriers": {
+    "text": "Reduce time barriers", 
+    "lm_type": "activities", 
+    "expanded_text": "Add weekend preK programs", 
+    "grant_type": "First5"
+  }, 
+  "activities_learn_bp": {
+    "text": "Learning/best practices", 
+    "lm_type": "activities", 
+    "expanded_text": "Write/present best practices<br>Long-term impact evaluation", 
+    "grant_type": "both"
+  }, 
+  "activities_preK_liveartist": {
+    "text": "Add live artist or performing art component to current programs", 
+    "lm_type": "activities", 
+    "expanded_text": "none", 
+    "grant_type": "IMLS"
+  }, 
+  "activities_preKoutreach": {
+    "text": "Preschool outreach", 
+    "lm_type": "activities", 
+    "expanded_text": "none", 
+    "grant_type": "First5"
+  }
+};
+
